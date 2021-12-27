716dc8fe9a503dea5a1018402592a8cfb0902d37
dingdongg <82309121+dingdongg@users.noreply.github.com>
2021-12-27 02:57:47 -0700

    Edited comments for this class

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -3,7 +3,6 @@ package persistence;
 import org.json.JSONObject;
 
 // represents a class that can be written/saved to a JSON file
-// code in this class was referenced from JsonSerializationDemo in Phase 2, Task 4 of the personal project
 public interface Writable {
 
     // EFFECTS: returns this as a JSON object
